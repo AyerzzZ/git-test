eb436ad13c2f4ceda212e1a85728b33e227be928
AyerzzZ <124754124+AyerzzZ@users.noreply.github.com>
2023-02-08 11:39:41 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hot-fix test1");
         System.out.println("hot-fix test2");
         System.out.println("push test2");
+        System.out.println("push test3");
     }
 }
